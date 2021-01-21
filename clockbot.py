3eb3875e722e00ab3b73f0388dae7026707e04ac
WieeRd_PC <wowluke@naver.com>
2021-01-22 01:09:26 +0900

    Added shutdown command, so many error messages tho :(

--- clockbot.py
+++ clockbot.py
@@ -30,6 +30,24 @@ async def on_command_error(ctx, error):
         return
     raise error
 
+# Owner only commands
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.NotOwner):
+        await ctx.send("에러: WieeRd 전용 커맨드")
+
+@bot.command(name="종료")
+@commands.is_owner()
+async def shutdown(ctx):
+    print("Terminating program...")
+    await ctx.send("장비를 정지합니다")
+    sys.stderr = open(os.devnull, 'w')
+    sys.stdout = open(os.devnull, 'w')
+    await bot.logout()
+    try: exit()
+    except: pass
+
 # Testing range
 
 @bot.command()

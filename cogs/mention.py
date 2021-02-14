ecfd21ff1b70f2a128c1f11ab398599acf09ec73
WieeRd_PC <wowluke@naver.com>
2021-02-14 22:42:46 +0900

    Advanced mention: Can now interpret each token
    Working on advanced mention feature #2

--- cogs/mention.py
+++ cogs/mention.py
@@ -3,32 +3,75 @@ import asyncio
 import shlex
 from discord.ext import commands
 
-def parse_target(self, guild : discord.Guild, token : str):
-    if   token[0]=='"': # "role"
-        pass
-    elif token[0]=="'": # 'user'
-        pass
-    elif token[0]=="`": # `userid`
-        pass
-    else: # wtf
-        pass
+def bestmatch(key:str, doors:Iterable, lock:Callable = lambda x:x):
+    # Some psychos put emojis in role/username
+    # Making it hard to type exact name
+    # So I had to make this thing
+    # find exact or best match of string
+    candidates = []
+    for door in doors:
+        if key==lock(door):
+            # return exact match
+            return door
+        elif (index := lock(door).find(key)) != -1:
+            # save partial matches
+            candidates.append((index, door))
+    if len(candidates)>0:
+        # if key is "clock",
+        # door "clockbot" is selected over "overclock"
+        index, door = min(candidates, key=lambda x: x[0])
+        return door
+    else:
+        return None
+
+def get_target(token: str, guild: discord.Guild) -> set:
+    name = token[1:-1]
+    quote = token[0] # "role" / 'nickname' / `user#0000`
+    if quote=='"':
+        # @everyone/@here is actually channel-specific,
+        # only including people who can see the channel.
+        # Should this mimic that behavior too?
+        if name=="everyone":
+            return set(guild.members)
+        elif name=="here":
+            is_active = lambda x: x.status != discord.Status.offline
+            return set(filter(is_active, guild.members))
+        else:
+            role = bestmatch(name, guild.roles, lambda r: r.name)
+            if role!=None: return set(role.members)
+            else: return None
+    elif quote=="'":
+        get_nick = lambda m: m.nick if m.nick!=None else m.name
+        user = bestmatch(name, guild.members, get_nick)
+        if user!=None: return {user}
+        else: return None
+    elif quote=="`":
+        user = guild.get_member_named(name)
+        if user!=None: return {user}
+        else: return None
+    else:
+        raise TypeError("Expected target token")
+
+def split_tokens(arg: str) -> list:
+    lexer = shlex.shlex(arg)
+    lexer.quotes += '`'
+    return [t for t in lexer] # can raise ValueError
 
 class mention(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="멘션")
+    @commands.command()
     async def mention(self, ctx, *, args=None):
         if args==None:
-            return
-        lexer = shlex.shlex(args)
-        lexer.quotes += '`'
-        tokens = []
-        try:
-            for t in lexer:
-                tokens.append(t)
-        except ValueError:
-            pass
+        	await ctx.send("사용법: 아직 나도 몰라")
+        	return
+        tokens = split_tokens(args)
+        await ctx.send(f"Tokens: {tokens}")
+        for t in tokens:
+        	await ctx.send(f"Parsing token {t[1:-1]}")
+        	target = get_target(t, ctx.guild)
+        	await ctx.send(repr([m.name for m in target]))
 
 def setup(bot):
     bot.add_cog(mention(bot))

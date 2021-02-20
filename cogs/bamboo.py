26f853d5bb3b8752244b9bea1b21a87fca63cdd7
WieeRd_laptop <wowluke@naver.com>
2021-02-20 15:38:45 +0900

    Bamboo forest working

--- cogs/bamboo.py
+++ cogs/bamboo.py
@@ -27,13 +27,13 @@ class Bamboo(commands.Cog):
             await ctx.send("에러: 서버 관리자 전용 커맨드")
             return
         if   cmd=="설치":
-            self.add_forest(ctx)
-        elif cmd=="제거":
-            self.rm_forest(ctx)
+            await self.add_forest(ctx)
+        elif cmd=="철거":
+            await self.rm_forest(ctx)
         else:
-            await ctx.send("사용법: !대나무숲 설치/제거")
+            await ctx.send("사용법: !대나무숲 설치/철거")
 
-    def add_forest(self, ctx):
+    async def add_forest(self, ctx):
         if ctx.channel.id in forests:
             await ctx.send("이미 대나무숲으로 설정된 채널입니다")
             return
@@ -41,21 +41,24 @@ class Bamboo(commands.Cog):
             await ctx.send("에러: 봇에게 해당 채널의 메세지 관리 권한이 필요합니다")
             return
         forests.add(ctx.channel.id)
-        msg = await ctx.send(f"{ctx.channel.mention}이 대나무숲 채널로 설정되었습니다\n"
-                              "모든 메세지는 익명의 봇 메세지로 전환됩니다\n"
-                              "주의) 메세지를 보낸 순간은 누구인지 알 수 있습니다")
-        msg.pin()
+        msg = await ctx.send(f"채널이 대나무숲으로 설정되었습니다\n"
+                              "모든 메세지는 익명으로 전환됩니다\n"
+                              "주의: 전송 직후 잠시 이름이 드러납니다")
+        await msg.pin()
 
-    def rm_forest(self, ctx):
+    async def rm_forest(self, ctx):
         if not ctx.channel.id in forests:
             await ctx.send("대나무숲으로 설정된 채널이 아닙니다")
             return
         forests.remove(ctx.channel.id)
-        await ctx.send("대나무숲을 제거했습니다")
+        await ctx.send("대나무숲을 철거했습니다")
 
     @commands.Cog.listener(name='on_message')
     async def replace(self, msg):
-        pass
+        if (msg.channel.id in forests) and (msg.author!=self.bot.user):
+            txt = msg.content
+            await msg.delete() # could raise discord.Forbidden
+            await msg.channel.send("???: " + txt)
 
 def setup(bot):
     bot.add_cog(Bamboo(bot))

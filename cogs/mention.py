73c4fef390e527fa294fc565b8b81d1656623e83
WieeRd_PC <wowluke@naver.com>
2021-02-21 17:28:16 +0900

    added DMention command

--- cogs/mention.py
+++ cogs/mention.py
@@ -47,6 +47,33 @@ class mention(commands.Cog):
         else:
             await ctx.send("조건에 일치하는 유저가 없습니다")
 
+    @commands.command(name="DM멘션")
+    async def DMention(self, ctx, *, expression=None):
+        if ctx.guild==None:
+            await ctx.send("서버에서만 사용 가능한 명령어입니다")
+            return
+        if expression==None:
+            await ctx.send("사용법: !DM멘션 <조건식>")
+            return
+        try:
+            target = MemberFilter.parse(expression, ctx.guild)
+        except Exception as e:
+            await ctx.send(f"```{type(e).__name__}: {e.args[0]}```")
+            return
+        if len(target)>0:
+            who = ctx.author.mention
+            where = ctx.guild.name
+            url = ctx.message.jump_url
+            msg = f"{who}님이 [{where}]에서 당신을 멘션했습니다.\n바로가기: {url}"
+
+            send = lambda user: getattr(user, 'send')(msg)
+            await asyncio.gather(*map(send, target))
+            # for user in target:
+            #     await user.send(msg)
+            await ctx.send(f"{len(target)}명의 유저에게 DM으로 알림을 보냈습니다")
+        else:
+            await ctx.send("조건에 일치하는 유저가 없습니다")
+
 def setup(bot):
     bot.add_cog(mention(bot))
     print(f"{__name__} has been loaded")

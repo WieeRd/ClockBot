5f4ab63b761b237ac055560e238420797e8374bc
WieeRd_laptop <wowluke@naver.com>
2021-06-26 11:19:27 +0900

    has_permission error handler

--- clockbot.py
+++ clockbot.py
@@ -1,20 +1,52 @@
 import discord
 import asyncio
 import aiohttp
-import aiomysql
 import time
 import enum
 
-from discord import Permissions, Webhook
+from discord import Webhook
 from discord.ext import commands
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
-# TODO: at least print out permission error to see what's inside
 # TODO: owner_or_admin
-# TODO: ctx.code("content", lang="python")
 
-PERM_KR_NAME: Dict[Permissions, str] = {
-
+PERM_KR_NAME: Dict[str, str] = {
+    "add_reactions": "반응 추가",
+    "administrator": "관리자",
+    "attach_files": "파일 첨부",
+    "ban_members": "멤버 차단",
+    "change_nickname": "별명 변경",
+    "connect": "음성채널 참가",
+    "create_instant_invite": "초대코드 생성",
+    "deafen_members": "멤버 헤드셋 음소거",
+    "embed_links": "링크 첨부",
+    "external_emojis": "외부 이모티콘 사용",
+    "kick_members": "멤버 추방",
+    "manage_channels": "채널 관리",
+    "manage_emojis": "이모티콘 관리",
+    "manage_guild": "서버 관리",
+    "manage_messages": "메세지 관리",
+    "manage_nicknames": "별명 관리",
+    "manage_permissions": "역할 관리",
+    "manage_roles": "역할 관리",
+    "manage_webhooks": "웹후크 관리",
+    "mention_everyone": "@\u200beveryone 멘션",
+    "move_members": "음성채널 멤버 이동",
+    "mute_members": "멤버 마이크 음소거",
+    "priority_speaker": "우선 발언권",
+    "read_message_history": "메세지 기록 보기",
+    "read_messages": "메세지 보기",
+    "request_to_speak": "스테이지 채널 발언권 요청",
+    "send_messages": "메세지 보내기",
+    "send_tts_messages": "TTS 메세지 전송",
+    "speak": "말하기",
+    "stream": "방송하기",
+    "use_external_emojis": "외부 이모티콘 사용",
+    "use_slash_commands": "빗금 명령어 사요",
+    "use_voice_activation": "음성 감지 사용",
+    "view_audit_log": "감사 로그 보기",
+    "view_channel": "채널 보기",
+    "view_guild_insights": "서버 인사이트 보기",
 }
 
 class ExitOpt(enum.IntFlag):
@@ -89,6 +121,8 @@ class ClockBot(commands.Bot):
         self.webhooks: Dict[int, Webhook] = {}
         # special channels (ex: bamboo forest) { channel_id : "reason" }
         self.specials: Dict[int, str] = {}
+        # dumped context objects for debugging
+        self.dumped: List[MacLak] = []
 
     async def close(self):
         await super().close()
@@ -129,16 +163,24 @@ class ClockBot(commands.Bot):
 
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
-            pass
+            pass # TODO maybe this can be used to send help when mentioned
         elif isinstance(error, commands.NoPrivateMessage):
-            await ctx.send("에러: 해당 명령어는 서버에서만 사용할 수 있습니다")
+            await ctx.code("에러: 해당 명령어는 서버에서만 사용할 수 있습니다")
         elif isinstance(error, commands.PrivateMessageOnly):
-            await ctx.send("에러: 해당 명령어는 DM에서만 사용할 수 있습니다")
+            await ctx.code("에러: 해당 명령어는 DM에서만 사용할 수 있습니다")
         elif isinstance(error, commands.BotMissingPermissions):
-            print(f"BotMissingPermissions: {error.missing_perms}") # TODO missing_perms are List[str] btw
+            perm_lst = ', '.join([PERM_KR_NAME[str(perm)] for perm in error.missing_perms])
+            many = '들' if len(perm_lst)>1 else ''
+            await ctx.code(f"에러: 봇에게 다음 권한{many}이 필요합니다: {perm_lst}")
         elif isinstance(error, commands.MissingPermissions):
-            print(f"MissingPermissions: {error.missing_perms}")
-        else: # TODO: Proper logging
-            print("***Something went wrong!***")
-            print(f"Caused by: {ctx.message.content}")
+            perm_lst = ', '.join([PERM_KR_NAME[str(perm)] for perm in error.missing_perms])
+            many = '들' if len(perm_lst)>1 else ''
+            await ctx.code(f"에러: 다음 권한{many}이 필요합니다: {perm_lst}")
+        elif isinstance(error, commands.CommandOnCooldown):
+            pass # TODO
+        else:
+            print(f"Unknown Error by: {ctx.message.content}")
             print(f"{type(error).__name__}: {error}")
+            print(f"Dumping context #{len(self.dumped)}")
+            self.dumped.append(ctx)
+            await ctx.code("에러: 알려지지 않은 오류가 발생했습니다")

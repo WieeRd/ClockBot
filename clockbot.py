9ab56b329dc229d9d44bc5a4d51620f2e284be2f
WieeRd_laptop <wowluke@naver.com>
2021-08-06 14:11:08 +0900

    Fixed owner_or_admin decorator

--- clockbot.py
+++ clockbot.py
@@ -51,10 +51,10 @@ PERM_KR_NAME: Dict[str, str] = {
 def owner_or_permissions(**perms):
     """bot owner or has_permissions"""
     original = commands.has_permissions(**perms).predicate
-    async def extended_check(ctx):
-        if ctx.guild is None:
+    async def extended_check(ctx: commands.Context):
+        if not ctx.guild:
             return False
-        return ctx.guild.owner_id == ctx.author.id or await original(ctx)
+        return await ctx.bot.is_owner(ctx.author) or await original(ctx)
     return commands.check(extended_check)
 
 def owner_or_admin():

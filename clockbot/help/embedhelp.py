b315444c6331cbb2c7456d83faf9b16d89d2c78e
WieeRd_laptop <wowluke@naver.com>
2021-09-05 23:29:51 +0900

    Much improvements

--- clockbot/help/embedhelp.py
+++ clockbot/help/embedhelp.py
@@ -5,7 +5,12 @@ import clockbot
 
 from discord.ext import commands
 from discord.ext.commands import Bot, Cog, Group, Command
-from typing import Dict, List
+from typing import Dict, List, Union, overload
+
+NO_HELP = "도움말이 작성되지 않았습니다"
+MORE_INFO = "자세한 정보"
+
+HelpObj = Union[None, Cog, Group, Command]
 
 def hoverlink(text: str, url: str, hover: str = '') -> str:
     return f"[{text}]({url} '{hover}')"
@@ -39,8 +44,10 @@ class EmbedHelp(commands.HelpCommand):
         return self.context.bot
 
     @property
-    def invoker(self) -> str:
-        return f"{self.clean_prefix}{self.invoked_with}"
+    def help_usage(self) -> str:
+        prefix = self.clean_prefix
+        usage = self.command_attrs.get('usage') or ''
+        return f"{prefix}{self.invoked_with} {usage}"
 
     def cmd_usage(self, cmd: Command) -> str:
         prefix = self.clean_prefix
@@ -90,7 +97,8 @@ class EmbedHelp(commands.HelpCommand):
         embed = discord.Embed(color=self.color, url=self.url)
 
         embed.title = self.title
-        embed.description = f"자세한 정보: `{self.invoker} <카테고리/명령어>`"
+        embed.description = f"`{self.help_usage}`"
+        # embed.description = f"{MORE_INFO}: `{self.help_usage}`"
         embed.set_thumbnail(url=self.thumbnail or str(self.bot.user.avatar_url))
 
         if self.tips:
@@ -100,7 +108,7 @@ class EmbedHelp(commands.HelpCommand):
         for icon, cog in mapping.items():
             embed.add_field(
                 name = f"{icon} {cog.qualified_name}",
-                value = cog.description or "도움말이 작성되지 않았습니다",
+                value = cog.description or NO_HELP,
                 inline = False
             )
 
@@ -110,15 +118,15 @@ class EmbedHelp(commands.HelpCommand):
         embed = discord.Embed(color=self.color, url=self.url)
 
         embed.title = f"{self.get_icon(cog)} {cog.qualified_name} 카테고리"
-        embed.description = f"**{cog.description}**"
-        # embed.set_thumbnail(url=self.thumbnail or str(self.bot.user.avatar_url))
-        embed.set_footer(text=f"자세한 정보: {self.invoker} <명령어>")
+        embed.description = f"**{cog.description or NO_HELP}**"
+        embed.set_footer(text=f"{self.help_usage}")
+        # embed.set_footer(text=f"{MORE_INFO}: {self.help_usage}")
 
         for cmd in cog.get_commands(): # set 'showcase' attr for custom order
             usage = self.cmd_usage(cmd)
             embed.add_field(
                 name = f"`{usage}`",
-                value = cmd.short_doc,
+                value = cmd.short_doc or NO_HELP,
                 inline = False
             )
 
@@ -129,28 +137,30 @@ class EmbedHelp(commands.HelpCommand):
 
         embed.set_author(name=f"카테고리: {grp.cog_name or '없음'}")
         embed.title = f"{self.clean_prefix}{grp.qualified_name}"
-        embed.description = f"**{grp.help or '도움말이 작성되지 않았습니다'}**"
-        embed.set_footer(text=f"자세한 정보: {self.invoker} {grp.qualified_name} <명령어>")
+        embed.description = f"**{grp.help or NO_HELP}**"
+        embed.set_footer(text=f"{self.help_usage}")
+        # embed.set_footer(text=f"{MORE_INFO}: {self.help_usage}")
 
         for cmd in grp.commands:
             usage = self.cmd_usage(cmd)
             embed.add_field(
                 name = f"`{usage}`",
-                value = cmd.short_doc or '도움말이 작성되지 않았습니다',
+                value = cmd.short_doc or NO_HELP,
                 inline = False
             )
 
         return embed
 
     # TODO: command check field (perm, cooldown)
-    def cmd_page(self, cmd: Command) -> discord.Embed:
+    def command_page(self, cmd: Command) -> discord.Embed:
         embed = discord.Embed(color=self.color, url=self.url)
 
         embed.set_author(name=f"카테고리: {cmd.cog_name or '없음'}")
         embed.title = f"{self.cmd_usage(cmd)}"
-        embed.set_footer(text=f"카테고리 더보기: {self.invoker} {cmd.cog_name or ''}")
+        embed.set_footer(text=f"{self.help_usage}")
+        # embed.set_footer(text=f"{MORE_INFO}: {self.help_usage}")
 
-        description = f"```{cmd.help or '도움말이 작성되지 않았습니다'}```"
+        description = f"```{cmd.help or NO_HELP}```"
         if cmd.aliases and not (
             isinstance(cmd, clockbot.AliasAsArg) or
             isinstance(cmd, clockbot.AliasGroup)
@@ -178,20 +188,22 @@ class EmbedHelp(commands.HelpCommand):
         await destin.send(embed=embed)
 
     async def send_command_help(self, cmd: Command):
-        embed = self.cmd_page(cmd)
+        embed = self.command_page(cmd)
         destin = self.get_destination()
         await destin.send(embed=embed)
 
     async def command_not_found(self, cmd: str) -> discord.Embed:
+        prefix = self.clean_prefix
         embed = discord.Embed(color = self.color)
         embed.set_author(name=f"명령어/카테고리 '{cmd}'를 찾을 수 없습니다")
-        embed.description = f"전체 목록 확인: `{self.invoker}`"
+        embed.description = f"전체 목록 확인: `{prefix}{self.invoked_with}`"
         return embed
 
     async def subcommand_not_found(self, cmd: Command, sub: str) -> discord.Embed:
+        prefix = self.clean_prefix
         embed = discord.Embed(color = self.color)
         embed.set_author(name=f"하위 명령어 '{sub}'를 찾을 수 없습니다")
-        embed.description = f"전체 목록 확인: `{self.invoker} {cmd.name}`"
+        embed.description = f"전체 목록 확인: `{prefix}{self.invoked_with} {cmd.name}`"
         return embed
 
     # TODO: fuzzy suggestion
@@ -206,12 +218,14 @@ class EmbedMenu(EmbedHelp):
     """
 
     mapping: Dict[str, Cog]
-    cached: Dict[str, discord.Embed]
+    cached: Dict[HelpObj, discord.Embed]
     msg: discord.Message
-    cursor: str
+    cursor: HelpObj
+    partial: bool
 
     def __init__(self, *,
             main: str = "\N{BOOKMARK}",
+            exit: str = "\N{CROSS MARK}",
             timeout: float = 60,
             inactive: int = 0x000000,
             **kwargs
@@ -219,65 +233,162 @@ class EmbedMenu(EmbedHelp):
         super().__init__(**kwargs)
         self.cached = {}
         self.main = main
+        self.exit = exit
         self.timeout = timeout
         self.inactive = inactive
 
-    def get_page(self, icon: str) -> discord.Embed:
-        if cached := self.cached.get(icon):
-            embed = cached
-        elif icon==self.main:
-            # opening bot page for the first time
-            mapping = self.get_bot_mapping()
-            embed = self.bot_page(mapping)
-            self.mapping = mapping
-            self.cached[self.main] = embed
-        elif cog := self.mapping.get(icon):
-            embed = self.cog_page(cog)
-            self.cached[icon] = embed
-        else:
-            raise ValueError("No corresponding page to icon")
+    @property
+    def help_usage(self) -> str:
+        return super().help_usage + " or 반응 추가"
+
+    def get_page(self, obj: HelpObj) -> discord.Embed:
+        if cached := self.cached.get(obj):
+            return cached
+
+        elif obj == None:
+            self.mapping = self.get_bot_mapping()
+            embed = self.bot_page(self.mapping)
+        elif isinstance(obj, Cog):
+            embed = self.cog_page(obj)
+        elif isinstance(obj, Group):
+            embed = self.group_page(obj)
+        elif isinstance(obj, Command):
+            embed = self.command_page(obj)
+        else: raise TypeError(f"{obj.__class__.__name__} is not HelpObj")
+
+        self.cached[obj] = embed
         return embed
 
-    async def button_handler(self, timeout: float):
+    @overload
+    def get_higher_being(self, obj: None) -> None: ...
+
+    @overload
+    def get_higher_being(self, obj: Cog) -> None: ...
+
+    @overload
+    def get_higher_being(self, obj: Group) -> Cog: ...
+
+    @overload
+    def get_higher_being(self, obj: Command) -> Union[Group, Cog]: ...
+
+    def get_higher_being(self, obj: HelpObj) -> HelpObj:
+        if obj == None:
+            return None
+        elif isinstance(obj, Cog):
+            return None
+        elif isinstance(obj, Command):
+            return obj.parent or obj.cog
+        else:
+            raise TypeError
+
+    async def menu_handler(self, timeout: float):
+        """
+        Handles reaction button interaction
+        """
         def check(reaction: discord.Reaction, user: discord.User) -> bool:
             return (
                 reaction.message == self.msg and
                 user == self.context.author and
                 ( reaction.emoji == self.main or
+                  reaction.emoji == self.exit or
                   reaction.emoji in self.mapping )
             )
-
+            
         while True:
-            reaction, user = await self.bot.wait_for(
-                'reaction_add',
-                check=check,
-                timeout=timeout,
-            )
-            await reaction.remove(user)
-
-            if self.cursor != reaction.emoji:
-                self.cursor = reaction.emoji
-                embed = self.get_page(reaction.emoji)
+            try:
+                reaction, user = await self.bot.wait_for(
+                    'reaction_add',
+                    check = check,
+                    timeout = timeout
+                )
+            except asyncio.TimeoutError:
+                embed = self.get_page(self.cursor)
+                embed.color = self.inactive
+                await self.msg.clear_reactions()
                 await self.msg.edit(embed=embed)
+                return
+
+            await reaction.remove(user)
+            icon = reaction.emoji
+
+            if icon == self.exit:
+                await self.msg.delete()
+                return
+            elif icon == self.main:
+                obj = self.get_higher_being(self.cursor)
+            else:
+                obj = self.mapping[icon] # mapping might not exist at this point
+
+            if obj == self.cursor:
+                continue
+
+            self.cursor = obj
+            embed = self.get_page(obj)
+            await self.msg.edit(embed=embed)
+
+            if obj==None and self.partial:
+                self.partial = False
+                for emoji in self.mapping:
+                    await self.msg.add_reaction(emoji)
 
     async def send_bot_help(self, mapping: Dict[str, Cog]):
         embed = self.bot_page(mapping)
         destin = self.get_destination()
         msg = await destin.send(embed=embed)
 
-        self.mapping = mapping
-        self.cached[self.main] = embed
-        self.msg = msg
-        self.cursor = self.main
-
+        self.partial = False
         await msg.add_reaction(self.main)
         for icon in mapping:
             await msg.add_reaction(icon)
 
-        try:
-            await self.button_handler(timeout=self.timeout)
-        except asyncio.TimeoutError:
-            embed = self.get_page(self.cursor)
-            embed.color = self.inactive
-            await self.msg.clear_reactions()
-            await self.msg.edit(embed=embed)
+        self.mapping = mapping
+        self.cached[None] = embed
+        self.msg = msg
+        self.cursor = None
+
+        await self.menu_handler(timeout=self.timeout)
+
+    async def send_cog_help(self, cog: Cog):
+        embed = self.cog_page(cog)
+        destin = self.get_destination()
+        msg = await destin.send(embed=embed)
+
+        self.partial = True
+        await msg.add_reaction(self.main)
+        await msg.add_reaction(self.exit)
+
+        self.cached[cog] = embed
+        self.msg = msg
+        self.cursor = cog
+
+        await self.menu_handler(timeout=self.timeout)
+
+    async def send_group_help(self, grp: Group):
+        embed = self.group_page(grp)
+        destin = self.get_destination()
+        msg = await destin.send(embed=embed)
+
+        self.partial = True
+        await msg.add_reaction(self.main)
+        await msg.add_reaction(self.exit)
+
+        self.cached[grp] = embed
+        self.msg = msg
+        self.cursor = grp
+
+        await self.menu_handler(timeout=self.timeout)
+
+    async def send_command_help(self, cmd: Command):
+        embed = self.command_page(cmd)
+        destin = self.get_destination()
+        msg = await destin.send(embed=embed)
+
+        self.partial = True
+        await msg.add_reaction(self.main)
+        await msg.add_reaction(self.exit)
+
+        self.cached[cmd] = embed
+        self.msg = msg
+        self.cursor = cmd
+
+        await self.menu_handler(timeout=self.timeout)
